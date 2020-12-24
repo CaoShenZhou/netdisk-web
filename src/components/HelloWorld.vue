45a3d8b0e17b33f2892add81e28586f903081727
caoshenzhou <1791915772@qq.com>
2020-12-24 11:16:29 +0800

    删除了无用的文件

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,20 @@
 <template>
   <v-container>
     <v-row class="text-center">
-      <v-col cols="12">
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        />
-      </v-col>
+      <v-col cols="12"> </v-col>
 
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
 
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
           <br />please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank"
-            >Discord Community</a
-          >
+          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
         </p>
       </v-col>
 
       <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
+        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
 
         <v-row justify="center">
           <a
@@ -43,9 +30,7 @@
       </v-col>
 
       <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
+        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
 
         <v-row justify="center">
           <a
@@ -61,9 +46,7 @@
       </v-col>
 
       <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
+        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
 
         <v-row justify="center">
           <a
@@ -89,53 +72,53 @@ export default {
     ecosystem: [
       {
         text: "vuetify-loader",
-        href: "https://github.com/vuetifyjs/vuetify-loader"
+        href: "https://github.com/vuetifyjs/vuetify-loader",
       },
       {
         text: "github",
-        href: "https://github.com/vuetifyjs/vuetify"
+        href: "https://github.com/vuetifyjs/vuetify",
       },
       {
         text: "awesome-vuetify",
-        href: "https://github.com/vuetifyjs/awesome-vuetify"
-      }
+        href: "https://github.com/vuetifyjs/awesome-vuetify",
+      },
     ],
     importantLinks: [
       {
         text: "Documentation",
-        href: "https://vuetifyjs.com"
+        href: "https://vuetifyjs.com",
       },
       {
         text: "Chat",
-        href: "https://community.vuetifyjs.com"
+        href: "https://community.vuetifyjs.com",
       },
       {
         text: "Made with Vuetify",
-        href: "https://madewithvuejs.com/vuetify"
+        href: "https://madewithvuejs.com/vuetify",
       },
       {
         text: "Twitter",
-        href: "https://twitter.com/vuetifyjs"
+        href: "https://twitter.com/vuetifyjs",
       },
       {
         text: "Articles",
-        href: "https://medium.com/vuetify"
-      }
+        href: "https://medium.com/vuetify",
+      },
     ],
     whatsNext: [
       {
         text: "Explore components",
-        href: "https://vuetifyjs.com/components/api-explorer"
+        href: "https://vuetifyjs.com/components/api-explorer",
       },
       {
         text: "Select a layout",
-        href: "https://vuetifyjs.com/getting-started/pre-made-layouts"
+        href: "https://vuetifyjs.com/getting-started/pre-made-layouts",
       },
       {
         text: "Frequently Asked Questions",
-        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions"
-      }
-    ]
-  })
+        href: "https://vuetifyjs.com/getting-started/frequently-asked-questions",
+      },
+    ],
+  }),
 };
 </script>
