c264b50a71c24fd8bea87589cbb723f287f4b0d7
caoshenzhou <1791915772@qq.com>
2020-12-24 10:46:26 +0800

    删除了无用的代码

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <v-app>
+    <!-- 页头 -->
     <v-app-bar app color="primary" dark>
       <v-toolbar-title>曹贼网盘</v-toolbar-title>
       <v-spacer></v-spacer>
@@ -8,10 +9,12 @@
       </v-btn>
     </v-app-bar>
 
+    <!-- 主体 -->
     <v-main>
       <router-view> </router-view>
     </v-main>
 
+    <!-- 页脚 -->
     <v-footer fixed padless dark>
       <v-card flat tile width="100%" class="primary text-center">
         <v-btn v-for="link in links" :key="link" color="white" text rounded class="my-2">
@@ -24,9 +27,7 @@
             </v-icon>
           </v-btn>
         </v-card-text>
-
         <v-divider></v-divider>
-
         <v-card-text class="white--text">
           {{ new Date().getFullYear() }} — <strong>Mr.Cao</strong>
         </v-card-text>
@@ -44,8 +45,10 @@ export default {
   data: () => ({
     icons: ["mdi-github", "mdi-qqchat", "mdi-wechat", "mdi-google"],
     links: ["首页", "关于", "团队", "服务", "博客", "联系"],
-    drawer: false,
-    group: null,
+    items: [
+      { title: "Home", icon: "mdi-view-dashboard" },
+      { title: "About", icon: "mdi-forum" },
+    ],
   }),
 };
 </script>
