debcf8fcd3fcd8fa8d878bc285cc0c8954c66a3c
caoshenzhou <1791915772@qq.com>
2020-12-25 14:06:57 +0800

    提交修改

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,92 @@
 <template>
   <v-app>
     <!-- 页头 -->
-    <v-app-bar app color="primary" dark>
-      <v-toolbar-title>
-        <router-link to="/" title>曹贼网盘</router-link>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn icon><v-icon>mdi-account-cog</v-icon></v-btn>
-      <v-btn icon><v-icon>mdi-exit-to-app</v-icon></v-btn>
+    <v-app-bar app flat color="primary" dark elevation="2">
+      <v-container class="py-0 fill-height d-none d-md-flex">
+        <v-avatar class="mr-10" color="blue darken-1" size="38"></v-avatar>
+        <v-btn v-for="link in links" :key="link" text>
+          {{ link }}
+        </v-btn>
+        <v-spacer></v-spacer>
+        <v-responsive max-width="260">
+          <v-text-field
+            dense
+            flat
+            hide-details
+            rounded
+            solo-inverted
+          ></v-text-field>
+        </v-responsive>
+      </v-container>
+      <v-container style="width: unset" class="d-flex d-md-none">
+        <v-btn text>曹贼网盘</v-btn>
+      </v-container>
     </v-app-bar>
 
     <!-- 主体 -->
-    <v-main>
-      <router-view> </router-view>
+    <v-main style="background: #eeeeee">
+      <v-container>
+        <v-row>
+          <v-col cols="12" md="2" lg="2" xl="2">
+            <v-card rounded elevation="2">
+              <v-list nav dense>
+                <v-list-item-group color="primary">
+                  <v-list-item>
+                    <v-list-item-icon>
+                      <v-icon>mdi-account-cog</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>账号设置</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-divider class="my-2"></v-divider>
+                  <v-list-item>
+                    <v-list-item-icon>
+                      <v-icon>mdi-star</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>我的收藏</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-icon>
+                      <v-icon>mdi-shield-lock</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>保险箱</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                  <v-list-item>
+                    <v-list-item-icon>
+                      <v-icon>mdi-delete</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>回收站</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+                <v-divider class="my-2"></v-divider>
+                <v-list-item-group>
+                  <v-list-item>
+                    <v-list-item-icon>
+                      <v-icon>mdi-exit-to-app</v-icon>
+                    </v-list-item-icon>
+                    <v-list-item-content>
+                      <v-list-item-title>退出</v-list-item-title>
+                    </v-list-item-content>
+                  </v-list-item>
+                </v-list-item-group>
+              </v-list>
+            </v-card>
+          </v-col>
+
+          <v-col>
+            <v-card elevation="2">
+              <router-view />
+            </v-card>
+          </v-col>
+        </v-row>
+      </v-container>
     </v-main>
 
     <!-- 页脚 -->
@@ -28,13 +102,6 @@
         >
           {{ link }}
         </v-btn>
-        <v-card-text>
-          <v-btn v-for="icon in icons" :key="icon" class="mx-4" icon>
-            <v-icon size="24px">
-              {{ icon }}
-            </v-icon>
-          </v-btn>
-        </v-card-text>
         <v-divider></v-divider>
         <v-card-text class="white--text">
           {{ new Date().getFullYear() }} — <strong>Mr.Cao</strong>
@@ -51,12 +118,7 @@ export default {
   components: {},
 
   data: () => ({
-    icons: ["mdi-github", "mdi-qqchat", "mdi-wechat", "mdi-google"],
     links: ["首页", "关于", "团队", "服务", "博客", "联系"],
-    items: [
-      { title: "Home", icon: "mdi-view-dashboard" },
-      { title: "About", icon: "mdi-forum" },
-    ],
   }),
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,14 +1,70 @@
 <template>
   <div class="home">
-    <div style="width:400px;height:1000px;"></div>
+    <div style="width: 400px; height: 1080px">
+      <v-card width="400">
+        <v-app-bar flat color="rgba(0, 0, 0, 0)">
+          <v-app-bar-nav-icon></v-app-bar-nav-icon>
+
+          <v-spacer></v-spacer>
+
+          <v-btn icon>
+            <v-icon>mdi-dots-vertical</v-icon>
+          </v-btn>
+        </v-app-bar>
+
+        <v-card-title class="white--text mt-8">
+          <p class="ml-3">John Doe</p>
+        </v-card-title>
+
+        <v-card-text>
+          <div class="font-weight-bold ml-8 mb-2">Today</div>
+
+          <v-timeline align-top dense>
+            <v-timeline-item
+              v-for="message in messages"
+              :key="message.time"
+              :color="message.color"
+              small
+            >
+              <div>
+                <div class="font-weight-normal">
+                  <strong>{{ message.from }}</strong> @{{ message.time }}
+                </div>
+                <div>{{ message.message }}</div>
+              </div>
+            </v-timeline-item>
+          </v-timeline>
+        </v-card-text>
+      </v-card>
+    </div>
   </div>
 </template>
 
 <script>
-
 export default {
   name: "Home",
-  components: {
-  }
+  components: {},
+  data: () => ({
+    messages: [
+      {
+        from: "You",
+        message: `Sure, I'll see you later.`,
+        time: "10:42am",
+        color: "deep-purple lighten-1",
+      },
+      {
+        from: "John Doe",
+        message: "Yeah, sure. Does 1:00pm work?",
+        time: "10:37am",
+        color: "green",
+      },
+      {
+        from: "You",
+        message: "Did you still want to grab lunch today?",
+        time: "9:47am",
+        color: "deep-purple lighten-1",
+      },
+    ],
+  }),
 };
 </script>
